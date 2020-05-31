2fbd177354e0f4e0c2310daa24caf19a55a7f9e5
Vasiliy Rubanov <rubanovas2005@gmail.com>
2020-05-31 23:22:27 +0700

    Add func mem_cpy

--- src/strings.c
+++ src/strings.c
@@ -16,3 +16,12 @@ size_t slen(const char* str)
     }
     return i;
 }
+
+void* mem_cpy(void* dest, const void* src, size_t len)
+{
+    char* d = dest;
+    const char* s = src;
+    while (len--)
+        *d++ = *s++;
+    return dest;
+}
--- src/strings.h
+++ src/strings.h
@@ -7,5 +7,6 @@ char incorrect_sym[] = " .,!?-+=\"'\\/():;*<>[]{}#$%^&@_|\n\t";
 
 int scmp(const char* str1, const char* str2);
 size_t slen(const char* str);
+void* mem_cpy(void* dest, const void* src, size_t len);
 
 #endif
